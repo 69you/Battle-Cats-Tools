6c97d06e0d87b43ff58ab9f2aee04cef5f1b400b
侑介 <spark20060609@gmail.com>
2023-07-26 20:35:43 +0800

    bruh

--- main.py
+++
@@ -1,6 +0,0 @@
-files = (
-    ('002a4b18244f32d7833fd81bc833b97f.dat','{}_sale.tsv'),
-    ('09b1058188348630d98a08e0f731f6bd.dat','{}_gatya.tsv'),
-    ('408f66def075926baea9466e70504a3b.dat','{}_daily.tsv'),
-    ('523af537946b79c4f8369ed39ba78605.dat','{}_ad.json'),
-)
