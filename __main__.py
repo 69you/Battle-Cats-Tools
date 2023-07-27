05867b20692b4e54fb7432b57d00b1f1aeeec94f
侑介 <spark20060609@gmail.com>
2023-07-27 16:07:54 +0800

    adding bcu unit and propety the order

--- __main__.py
+++ __main__.py
@@ -1,6 +1,6 @@
 from pprint import pprint
-from utils_ import decrypt, adb, event_old, event_new, adb_event
 import inquirer, os, time, sys
+from utils_ import decrypt, adb, event_old, event_new, adb_event, bcu_unit
 
 def red(text) -> str:
     return('\033[31m' + text + '\033[0m')
@@ -32,31 +32,29 @@ def run(func):
 
 
 def main():
-    try:
-        match(ask("Please Select a mode", ["Decrypt a apk", "Get server file", "Get Event File", "Exit"])):
-            case "Decrypt a apk":
-                run(decrypt.decrypt)
-            case "Get server file":
-                run(adb.adb)
-            case "Get Event File":
-                os.system('cls')
-                match(ask("Please Select a mode", ["The old Way", "The new Way", "By ADB","Back"])):
-                    case "The old Way":
-                        run(event_old.event)
-                    case "The new Way":
-                        run(event_new.event)
-                    case "By ADB":
-                        run(adb_event.adb_event)
-                    case "Back":
-                        os.system('cls')
-                        main()
-            case "Exit":
-                print(green('Thanks for using!'))
-                time.sleep(2)
-                exit(0)
-    except KeyboardInterrupt:
-        os.system('exit')
-
+    match(ask("Please Select a mode", ["Decrypt a apk", "Get server file", "Get Event File", "Moving Unit to BCU","Exit"])):
+        case "Decrypt a apk":
+            run(decrypt.decrypt)
+        case "Get server file":
+            run(adb.adb)
+        case "Get Event File":
+            os.system('cls')
+            match(ask("Please Select a mode", ["The old Way", "The new Way", "By ADB","Back"])):
+                case "The old Way":
+                    run(event_old.event)
+                case "The new Way":
+                    run(event_new.event)
+                case "By ADB":
+                    run(adb_event.adb_event)
+                case "Back":
+                    os.system('cls')
+                    main()
+        case "Moving Unit to BCU":
+            run(bcu_unit.bcu_unit)
+        case "Exit":
+            print(green('Thanks for using!'))
+            time.sleep(2)
+            exit(0)
 
 
 if __name__ == "__main__":
